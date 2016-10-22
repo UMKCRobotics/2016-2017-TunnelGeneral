326ba6c1239ae3ef1d4b08debf683584c8b4f8d5
beauxq <beauxq@users.noreply.github.com>
2016-10-22 15:38:41 -0500

    documentation in static variable decorator

--- Competition Game/src/Static_Decorator.py
+++ Competition Game/src/Static_Decorator.py
@@ -1,7 +1,17 @@
-# a decorator for static variables in python
+# a decorator for function static variables in python
 def static_vars(**kwargs):
     def decorate(func):
         for k in kwargs:
             setattr(func, k, kwargs[k])
         return func
     return decorate
+
+"""
+example usage:
+
+@static_vars(my_static_variable=0)
+def my_function_with_a_static_variable():
+    my_function_with_a_static_variable.my_static_variable += 1
+    print(my_function_with_a_static_variable.my_static_variable)
+
+"""
