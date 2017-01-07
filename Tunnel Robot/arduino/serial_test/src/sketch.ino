cb1955247cd77c617ea8f1bb39ea5e79ae1124df
Kosinkadink <kosinkadink1@gmail.com>
2017-01-07 16:05:52 -0600

    serial testing sketch/python script added

---
+++ Tunnel Robot/arduino/serial_test/src/sketch.ino
@@ -0,0 +1,96 @@
+#define LED1 5
+#define LED2 6
+#define LED3 7
+
+//parsing inputs
+String command; //used to store command from serial
+String value; //used to store value from serial
+String previousValue;
+String response; //used to store response to main program
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+  pinMode(LED3,OUTPUT);
+  Serial.begin(115200);
+  Serial.write(1);
+  previousValue = "";
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  command = "";
+  value = "";
+  int addTo = 0; //0 for command, 1 for value
+  //ButtonStates();
+  if(Serial.available()){
+    while (Serial.available() > 0)
+    {
+      char readIn = (char)Serial.read();
+      if (readIn == '\n') {
+        break;
+      }
+      else if (readIn == '|') {
+        addTo = 1;
+        continue;
+      }
+      //other stuff that is important
+      if (addTo == 0) {
+        command += readIn;
+      }
+      else if (addTo == 1) {
+        value += readIn;
+      }
+    }
+    response = interpretCommand(command,value);
+    Serial.println(response+previousValue); //sends response with \n at the end
+    previousValue = value;
+  }
+  //small delay
+  delay(20);
+}
+
+String interpretCommand(String command, String value) {
+  
+  String responseString = "n";
+  String returnString = "";
+
+  //check if motor stuff
+  if (command == "L") {
+    returnString = getLED(value);
+    responseString = "1";
+    responseString += returnString;
+  }
+
+  return responseString;
+
+}
+
+String getLED(String value) {
+  if (value == "Y") {
+    digitalWrite(LED1,HIGH);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+  }
+  else if (value == "B") {
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED2,HIGH);
+    digitalWrite(LED3,LOW);
+  }
+  else if (value == "G") {
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,HIGH);
+  }
+  else if (value == "N") {
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+  }
+  else {
+    return "BAd"+value;
+  }
+  return "1ABCDEFGH"+value;
+}
---
+++ Tunnel Robot/led_test.py
@@ -0,0 +1,108 @@
+#get serial library for python-2.7.x
+import os,sys,time
+import serial
+
+#get robot + robotmap for general movement + navigation
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) #directory from which this script is ran
+sys.path.insert(0, os.path.join(__location__,'src/'))
+#insert location from Competition Game
+main_dir = os.path.realpath(os.path.join(__location__,'..'))
+sys.path.insert(0, os.path.realpath(os.path.join(main_dir,'Competition Game/src')))
+
+
+#from SensorArduino import EMF_Sensors
+#from Displays import Displays
+from ArduinoFuncs import ArduinoFuncs
+from Robot_Impl import Robot_Impl
+from DeviceComm import CommRequest
+from Robot import RobotMap
+from AI_JED import RobotAlg as AlgJed
+from AI_17 import Robot as Alg17
+
+ard_serial = serial.Serial('/dev/ttyACM0',115200)
+
+#motors = MotorsNXT()
+#emf = EMF_Sensors(emf_serial)
+#display  = Displays(disp_serial)
+ard_funcs = ArduinoFuncs(ard_serial)
+while not ard_funcs.ard.connected:
+	time.sleep(0.1)
+
+
+def wait_till_done(resp):
+        intermediateDelay = 0.01
+        while not resp.isDone:
+            time.sleep(intermediateDelay)
+        return resp.getResponse()
+
+display = None
+
+globalThreads = []
+
+#initialize Robot_Impl
+direction = 0
+robot_map = RobotMap(None,50,(0,0),direction,shouldPlaySound=False)
+robot_impl = Robot_Impl(ard_funcs,robot_map)
+#alg17:
+#robotAlgorithm = Alg17(robot_impl,display)
+#robotAlgorithm.explore3()
+
+#algJed:
+#robotAlgorithm = AlgJed(robot_impl,robot_impl)
+#robotAlgorithm.doStuff()
+print 'Ready for commands'
+resp = "1"
+automatic = True
+index = 0
+possibilities = ['y','b','g']
+while True:
+        if automatic:
+                if resp.startswith("BAD") or resp.startswith("BAd"):
+                        automatic = False
+                        print "was on: %s" % possibilities[index]
+                        print 'attempting to flush...'
+                        time.sleep(0.5)
+                        ard_funcs.ard.serial.flushInput()
+                        ard_funcs.ard.serial.flushOutput()
+                        continue
+                index = (index+1)%3
+                user_inp = possibilities[index]
+                time.sleep(0.001)
+	else:
+                user_inp = raw_input('> ')
+	if user_inp == 'y':
+                commandObj = CommRequest("L|Y")
+                ard_funcs.ard.requestCommand(commandObj)
+		resp = wait_till_done(commandObj)
+		print "%s: %s" % (user_inp,resp) 
+	elif user_inp == 'b':
+                commandObj = CommRequest("L|B")
+                ard_funcs.ard.requestCommand(commandObj)
+		resp = wait_till_done(commandObj)
+		print "%s: %s" % (user_inp,resp) 
+	elif user_inp == 'g':
+                commandObj = CommRequest("L|G")
+                ard_funcs.ard.requestCommand(commandObj)
+		resp = wait_till_done(commandObj)
+		print "%s: %s" % (user_inp,resp) 
+	elif user_inp == 'n':
+                commandObj = CommRequest("L|N")
+                ard_funcs.ard.requestCommand(commandObj)
+		resp = wait_till_done(commandObj)
+		print "%s: %s" % (user_inp,resp) 
+	elif user_inp == 'a':
+                automatic = True
+        elif user_inp == 'flush':
+                ard_funcs.ard.serial.flushInput()
+                ard_funcs.ard.serial.flushOutput()
+	elif user_inp == 'exit':
+		break
+        else:
+                commandObj = CommRequest("mmm")
+                ard_funcs.ard.requestCommand(commandObj)
+		resp = wait_till_done(commandObj)
+		print resp 
+	
+
+for thread in globalThreads:
+	thread.stopThread()
