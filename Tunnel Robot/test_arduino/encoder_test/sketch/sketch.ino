204e7d33887b45fb9d8d302ef5b280c05b04b214
beauxq <beauxq@users.noreply.github.com>
2017-03-15 15:21:46 -0500

    encoder test sketch

---
+++ Tunnel Robot/test_arduino/encoder_test/sketch/sketch.ino
@@ -0,0 +1,87 @@
+#include "Arduino.h"
+// https://www.dfrobot.com/wiki/index.php/12V_DC_Motor_251rpm_w/Encoder_(SKU:_FIT0186)
+
+// The sample code for driving one way motor encoder
+const byte rightEncoder0InterruptPinA = 2;  //A pin -> the interrupt pin 0
+const byte rightEncoder0DigitalPinB = 4;  //B pin -> the digital pin 4
+byte rightEncoder0IntPinALast;
+int rightDuration;  //the number of the pulses
+boolean rightDirection;  //the rotation direction
+
+const byte leftEncoder0InterruptPinA = 3;  //A pin
+const byte leftEncoder0DigitalPinB = 5;  //B pin
+byte leftEncoder0IntPinALast;
+int leftDuration;  //the number of the pulses
+boolean leftDirection;  //the rotation direction
+
+
+void setup()
+{
+  Serial.begin(115200);  // Initialize the serial port
+  EncoderInit();  // Initialize the module
+}
+
+void loop()
+{
+  Serial.print("Pulse: right: ");
+  Serial.print(rightDuration);
+  Serial.print(" left: ");
+  Serial.println(leftDuration);
+  rightDuration = 0;
+  leftDuration = 0;
+  delay(100);
+}
+
+void EncoderInit()
+{
+  rightDirection = true;  //default -> Forward
+  pinMode(rightEncoder0DigitalPinB,INPUT);
+    attachInterrupt(0, rightEncoderInterruptFunction, CHANGE);
+
+    leftDirection = true;  //default -> Forward
+    pinMode(leftEncoder0DigitalPinB,INPUT);
+    attachInterrupt(0, leftEncoderInterruptFunction, CHANGE);
+}
+
+/** original name "wheelSpeed" */
+void rightEncoderInterruptFunction()
+{
+  int Lstate = digitalRead(rightEncoder0InterruptPinA);
+  if((rightEncoder0IntPinALast == LOW) && Lstate==HIGH)
+  {
+    int val = digitalRead(rightEncoder0DigitalPinB);
+    if(val == LOW && rightDirection)
+    {
+      rightDirection = false; //Reverse
+    }
+    else if(val == HIGH && !rightDirection)
+    {
+      rightDirection = true;  //Forward
+    }
+  }
+  rightEncoder0IntPinALast = Lstate;
+
+  if(!rightDirection)  rightDuration++;
+  else  rightDuration--;
+}
+
+void leftEncoderInterruptFunction()
+{
+    int Lstate = digitalRead(leftEncoder0InterruptPinA);
+    if((leftEncoder0IntPinALast == LOW) && Lstate==HIGH)
+    {
+        int val = digitalRead(leftEncoder0DigitalPinB);
+        if(val == LOW && leftDirection)
+        {
+            leftDirection = false; //Reverse
+        }
+        else if(val == HIGH && !leftDirection)
+        {
+            leftDirection = true;  //Forward
+        }
+    }
+    leftEncoder0IntPinALast = Lstate;
+
+    if(!leftDirection)  leftDuration++;
+    else  leftDuration--;
+}
