fd2ff1cb8219ce8c7c9497cc17e40ef882e318cb
beauxq <beauxq@users.noreply.github.com>
2017-03-29 10:18:46 -0500

    save program output in log

--- main_launcher.sh
+++ main_launcher.sh
@@ -2,4 +2,4 @@
 # main_launcher.sh
 # runs main.py
 
-sudo python "/home/pi/2016-2017-TunnelGeneral/Tunnel Robot/main.py"
+sudo python "/home/pi/2016-2017-TunnelGeneral/Tunnel Robot/main.py" 2>&1 | tee "/home/pi/robotlogs/$(date +"%FT%H%M%S").log"
